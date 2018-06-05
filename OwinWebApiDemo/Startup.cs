e533cbea1db448617fc7a0c35989379e99c5e311
panyp <panyongpeng@foxmail.com>
2018-06-05 23:04:33 +0800

    提交基础代码及nuget命令

---
+++ OwinWebApiDemo/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System.Web.Http;
+
+namespace OwinWebApiDemo
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+
+                name: "DefaultRoute",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { controller = "Index", action = "Index", id = RouteParameter.Optional, }
+            );
+            appBuilder.UseWebApi(config);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            //client.PostAsync("", new System.Net.Http.StringContent("", Encoding.UTF8, ""));
+            using (WebApp.Start<Startup>("http://127.0.0.1:8080"))
+            //使用+可以绑定所有地址
+            //using (WebApp.Start<Startup>("http://+:8080"))
+            {
+                Console.WriteLine("Owin WebApp is running......");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 新建文本文档.txt.bat
@@ -0,0 +1,6 @@
+echo "# OwinWebApiDemo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/pondyond/OwinWebApiDemo.git
+git push -u origin master
